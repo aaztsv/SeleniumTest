f521ed70b9b6e75b6cfcfdd1f29df9d228ee7016
aaztsv <zaitseevaa@gmail.com>
2022-05-11 21:37:04 +0300

    Changes

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,26 +1,15 @@
 
 
+import Base.Base;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-public class FirstTest {
+public class FirstTest extends Base {
 
     @Test
 
     public void firstTest(){
-
-        System.setProperty("webdriver.chrome.driver", "/Users/nastiazaitseva/Downloads/chromedriver");
-
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        driver.manage().window().maximize();
 
         driver.get("https://www.google.com");
 
@@ -30,6 +19,5 @@ public class FirstTest {
 
         System.out.println(driver.getTitle());
 
-        driver.quit();
     }
 }
--- src/test/java/HwFive.java
+++ src/test/java/HwFive.java
@@ -1,46 +1,32 @@
 
+import Base.Base;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+public class HwFive extends Base {
 
-    public class HwFive {
+    @Test
+    public void firstTest() throws InterruptedException {
 
-        @Test
-        public void firstTest() throws InterruptedException {
+        driver.get("https://skims.com/account/login");
+        Thread.sleep(3000);
 
-            System.setProperty("webdriver.chrome.driver", "/Users/nastiazaitseva/Downloads/chromedriver");
+        WebElement Email = driver.findElement(By.id("customerEmail"));
+        Email.sendKeys("test@test.com");
+        Thread.sleep(3000);
 
-            WebDriver driver = new ChromeDriver();
+        WebElement password = driver.findElement(By.id("customerPassword"));
+        password.sendKeys("pass123");
+        Thread.sleep(3000);
 
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
-
-            driver.get("https://skims.com/account/login");
-            Thread.sleep(3000);
-
-
-            WebElement Email = driver.findElement(By.id("customerEmail"));
-            Email.sendKeys("test@test.com");
-            Thread.sleep(3000);
-
-            WebElement password = driver.findElement(By.id("customerPassword"));
-            password.sendKeys("pass123");
-            Thread.sleep(3000);
-
-            WebElement signinButton = driver.findElement(By.xpath("//button[contains(text(),'sign in')]"));
-            signinButton.click();
-            Thread.sleep(3000);
-
-            driver.quit();
-
-        }
+        WebElement signinButton = driver.findElement(By.xpath("//button[contains(text(),'sign in')]"));
+        signinButton.click();
+        Thread.sleep(3000);
 
     }
+
+}
 
 
 
